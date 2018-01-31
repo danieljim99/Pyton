10127d116b80fdc48363e22ae920f375d9e91e17
danieljim99 <34034368+danieljim99@users.noreply.github.com>
2018-01-31 19:01:47 +0100

    Create ejercicios.py

---
+++ ejercicios.py
@@ -0,0 +1,4 @@
+<nombre de variable> = numero (variable tipo int) //si es decimal debe ser con . no con , (variable tipo float/double) //n = 3.14
+<nombre de variable> = "caracter" (variable tipo char/String) //a = "hola
+<nombre de variable> = True/False (variable tipo booleana) //cond = True
+<tipo de variable>(<nombre de variable>) = convertir dicha variable al tipo indicado // int(a) // str(n)
